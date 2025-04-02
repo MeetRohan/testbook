9f8c714d18ad199f7ae2bb77b55586fc80489109
MeetRohan <raudramittal@gmail.com>
2025-04-02 17:01:37 +0530

    Removed .env file from history

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/admin-dashboard",
+        has: [
+          {
+            type: "cookie",
+            key: "next-auth.session-token",
+            value: "(.*)",
+          },
+        ],
+        permanent: false,
+        destination: "/login",
+      },
+    ];
+  },
+};
 export default nextConfig;
+//module.exports = nextConfig;
